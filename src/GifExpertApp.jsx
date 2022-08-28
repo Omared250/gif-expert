113021971f954e8b05bc91945e970724185c2af8
Omar <omared250@gmail.com>
2022-08-28 13:51:31 +0200

    comunication between components

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -6,7 +6,7 @@ export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
     const onAddCategory = () => {
-        setCategories(['Valorant', ...categories]);
+        setCategories([ 'Valorant', ...categories]);
     }
 
     return (
@@ -15,9 +15,8 @@ export const GifExpertApp = () => {
            <h1>GifExpertApp</h1>
 
             {/* Input */}
-            <AddCategory />
+            <AddCategory setCategories={ setCategories }/>
 
-            <button onClick={onAddCategory} >Add</button>
             {/* Gif List */}
             <ol>
                 { categories.map( category => <li key={category}>{category}</li>) }
