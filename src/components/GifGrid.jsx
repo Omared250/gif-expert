e89fd7e34757e8484ebf1acef08c8a48436f2655
Omar <omared250@gmail.com>
2022-08-28 18:13:03 +0200

    creating GifItem component

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { GifItem } from "./GifItem";
 import { getGifs } from "../helpers/getGifs"
 
 export const GifGrid = ({ category }) => {
@@ -17,11 +18,15 @@ export const GifGrid = ({ category }) => {
   return (
     <>
       <h3>{category}</h3>
-      <ol>
+      <div className="card-grid">
         {
-          images.map(({id, title}) => <li key={ id }>{ title }</li>)
+          images.map(( img ) => 
+            <GifItem key={ img.id }
+            { ...img }
+            />
+          )
         }
-      </ol>
+      </div>
     </>
   )
 }
